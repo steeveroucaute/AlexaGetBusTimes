5db0fcbdb3d437fefb7db8356c89644df27cc65f
steeveroucaute <steeve.roucaute@me.com>
2016-11-10 18:50:42 +0100

    implemented lodash for sorting array as no good with JS .sortBy

--- offline/apiReqTester/apiRequestTester.js
+++ offline/apiReqTester/apiRequestTester.js
@@ -1,30 +1,29 @@
+var _ = require('lodash/core');
 const request = require('request-promise');
-
+//configure call for request-promise
 const options = {
 	method: 'GET',
 	uri: ' https://api.tfl.gov.uk/StopPoint/490012165S/Arrivals?app_id=bb3622c2&app_key=1648a368a0db8678a6b34e5a387c6eb2',
 	json: true
 }
-
-// console.log(Math.floor(565/60));
-
-
+//GET arrival times from API
 request(options)
+	//success push into arrivals array 
 	.then(function (response) {
 		var arrivals = [];
-		// console.log(length);
 		for (var i in response) {
 			console.log("Arrival time bus "  + i + " : " + Math.floor(response[i].timeToStation/60) + " mins");
 			arrivals.push(Math.floor(response[i].timeToStation/60));
 		}
-		// console.log(Math.floor(response[1].timeToStation/60
-		console.log(arrivals.sort());
+		console.log(_.sortBy(arrivals));
 
 		console.log('SUCCESS');
 	})
+	//error handling
 	.catch(function(err){
 		console.log("ERROR\n" + err);
 	})
+
 
 
 
